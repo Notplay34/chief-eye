7c4f2ba8ad28f9022239ee48ee8342b7fde2108b
NotPlay <notplay@example.com>
2026-04-25 14:02:58 +0300

    revert: remove state duty qr payment

--- frontend/form-page/init.js
+++ frontend/form-page/init.js
@@ -140,7 +140,6 @@
     setInterval(page.updateTime, 60000);
 
     if (page.btnAddDoc) page.btnAddDoc.addEventListener('click', page.addSelectedDocument);
-    if (page.btnStateDutyQr) page.btnStateDutyQr.addEventListener('click', page.toggleStateDutyQr);
     if (page.btnAcceptCash) page.btnAcceptCash.addEventListener('click', page.acceptCash);
     if (page.btnPrint) page.btnPrint.addEventListener('click', page.doPrint);
     page.loadFormHistory();
